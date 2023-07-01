a11ebeed8b0c48fb95ba9fbe993242f1553869b7
Daddyjohn63 <john@webandprosper.co.uk>
2023-07-01 09:45:45 +0100

    dynamic params

---
+++ app/[category]/page.tsx
@@ -0,0 +1,24 @@
+import { DUMMY_CATEGORIES, DUMMY_POSTS } from '@/DUMMY_DATA';
+
+export const generateStaticParams = async () => {
+  return DUMMY_CATEGORIES.map(category => {
+    return {
+      category: category.slug
+    };
+  });
+};
+
+const Page = ({
+  params
+}: {
+  params: {
+    category: string;
+  };
+}) => {
+  const posts = DUMMY_POSTS.filter(
+    post => post.category.title.toLocaleLowerCase() === params.category
+  );
+  return <div>{JSON.stringify(posts)}</div>;
+};
+
+export default Page;
