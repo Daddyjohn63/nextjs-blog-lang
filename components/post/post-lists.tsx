e9af0c550b31c0307755f413dbe00587bc6cbc9c
Daddyjohn63 <john@webandprosper.co.uk>
2023-07-12 16:53:03 +0100

    get category data

--- components/post/post-lists.tsx
+++ components/post/post-lists.tsx
@@ -10,7 +10,7 @@ interface PostListProps {
 const PostList = ({ posts, layout = 'vertical' }: PostListProps) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-10 lg:grid-flow-col lg:auto-cols-fr">
-      {posts.map(post => (
+      {posts?.map(post => (
         <PostCard layout={layout} post={post} key={post.id} />
       ))}
     </div>
