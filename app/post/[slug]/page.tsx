453b71a44ec8c1479f55e70fbc73893504352670
Daddyjohn63 <john@webandprosper.co.uk>
2023-07-14 17:28:17 +0100

    lecture 67 complete

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -105,7 +105,7 @@ const Page = async ({
           <PostBody body={post.body} />
         </div>
         {/* CTA Card */}
-        {/* {@ts-expect-error Async Server Component} */}
+
         <CTACard />
       </div>
     </PaddingContainer>
